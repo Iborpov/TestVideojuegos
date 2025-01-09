a889c93caedb34ea5ccf25a958cb5c62d7daadbb
Adrián Iborra <32207929+Iborpov@users.noreply.github.com>
2025-01-09 19:50:16 +0100

    Jump Man start

--- Jump Man/Assets/Scripts/Player.cs
+++ Jump Man/Assets/Scripts/Player.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,15 +9,111 @@ public class Player : MonoBehaviour
     public enum PlayerControllerState
     {
         Idle,
-        Walk,
+        Run,
         Jump,
-        DubbleJump,
-        Wall,
+        DoubleJump,
+        OnWall,
     }
 
+    public enum PlayerImput
+    {
+        Left,
+        Rigth,
+        Jump,
+        NoImput,
+    }
+
+    Animator animator;
+
+    PlayerControllerState state = PlayerControllerState.Idle;
+    PlayerImput imput = PlayerImput.NoImput;
+
     // Start is called before the first frame update
-    void Start() { }
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        GetInput();
+        //Controlar la orientación
+
+        //Establecer parámetros del animator
+
+        if (imput == PlayerImput.Rigth)
+        {
+            animator.SetBool("Running", true);
+        }
+    }
+
+    private void GetInput()
+    {
+        //Leer inputs del jugador (derecha/izquierda y salto)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            imput = PlayerImput.Jump;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            imput = PlayerImput.Left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            imput = PlayerImput.Rigth;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        switch (state)
+        {
+            case PlayerControllerState.Idle:
+                Idle();
+                break;
+            case PlayerControllerState.Run:
+                Run();
+                break;
+            case PlayerControllerState.Jump:
+                Jump();
+                break;
+            case PlayerControllerState.DoubleJump:
+                DoubleJump();
+                break;
+            case PlayerControllerState.OnWall:
+                OnWall();
+                break;
+        }
+    }
+
+    private void Idle()
+    {
+        //Comprobar si tengo que cambiar de estado
+        //Mover al player
+    }
+
+    private void Run()
+    {
+        //Comprobar si tengo que cambiar de estado
+        //Mover al player
+    }
+
+    private void Jump()
+    {
+        //Comprobar si tengo que cambiar de estado
+        //Mover al player
+    }
+
+    private void DoubleJump()
+    {
+        //Comprobar si tengo que cambiar de estado
+        //Mover al player
+    }
+
+    private void OnWall()
+    {
+        //Comprobar si tengo que cambiar de estado
+        //Mover al player
+    }
 }
--- Jump Man/Assets/Scripts/SoundManager.cs
+++ Jump Man/Assets/Scripts/SoundManager.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
+    void Update() { }
+
+    public void PlayClip(AudioClip clip)
     {
-        
+        GetComponent<AudioSource>().PlayOneShot(clip);
     }
 }
