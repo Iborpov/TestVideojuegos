be1ff0243757656a35c42a2c1b8b53ebfc1b45b8
Adrián Iborra <32207929+Iborpov@users.noreply.github.com>
2025-01-30 19:55:09 +0100

    room1 preset

--- The Binding of Pep/Assets/Scripts/CameraManager.cs
+++ The Binding of Pep/Assets/Scripts/CameraManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    float currentPosx;
+    float currentPosy;
+
+    Vector3 velocity;
+    public float speed = 5;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Awake() { }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            new Vector3(currentPosx, currentPosy, transform.position.z),
+            ref velocity,
+            speed
+        );
+    }
+
+    public void MoveToNewRoom(Transform newRoom)
+    {
+        currentPosx = newRoom.position.x;
+        currentPosy = newRoom.position.y;
     }
 }
--- The Binding of Pep/Assets/Scripts/ProcedManager.cs
+++ The Binding of Pep/Assets/Scripts/ProcedManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class ProcedManager : MonoBehaviour
 {
+    [SerializeField]
+    List<GameObject> rooms;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 }
